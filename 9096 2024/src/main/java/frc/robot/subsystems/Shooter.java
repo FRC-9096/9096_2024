68c3bc97ceb83fb93934e02de64d4bb22e8a8e4d
henry <63601449+HenryBass@users.noreply.github.com>
2024-03-09 13:51:49 -0500

    Update Shooter.java

--- 9096 2024/src/main/java/frc/robot/subsystems/Shooter.java
+++ 9096 2024/src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+//import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 //import edu.wpi.first.wpilibj2.command.Command;
 //import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
