374a3be9c678be11ac1bc26e6e1035df1d40040d
henry <63601449+HenryBass@users.noreply.github.com>
2024-03-12 17:12:24 -0400

    Update ReloadLauncher.java

--- 9096 2024/src/main/java/frc/robot/commands/ReloadLauncher.java
+++ 9096 2024/src/main/java/frc/robot/commands/ReloadLauncher.java
@@ -1,10 +1,12 @@
 package frc.robot.commands;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Reload;
 import frc.robot.RobotContainer;
 
 public class ReloadLauncher extends Command {
     public final Reload m_reload;
+    public PIDController PID;
     //public final Reload Reloading;
     
     
